d53d3b7aa5ca6b19cfeaecef40b1c40441b23ab9
GuGyoSong <1091916454@qq.com>
2026-05-23 11:31:24 +0800

    Update qr_decoder.py

--- qr_decoder.py
+++ qr_decoder.py
@@ -1,9 +1,15 @@
+import cv2
 from pyzbar.pyzbar import decode
 
+
 def decode_qr(image):
-    """
-    输入: OpenCV图像
-    输出: QR内容列表
-    """
+
     results = decode(image)
-    return [r.data.decode("utf-8") for r in results]
+
+    qr_list = []
+
+    for r in results:
+        qr_data = r.data.decode("utf-8")
+        qr_list.append(qr_data)
+
+    return qr_list
